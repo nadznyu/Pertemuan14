adfc544e48fc875119c185eff9fa397a0ed683ca
nadznyu <nadiaalya0107@gmail.com>
2025-12-03 11:35:55 +0700

    Modifikasi Percobaan 3

--- Kafe17.java
+++ Kafe17.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
 
 public class Kafe17 {
-    public static void main(String[] args) {
-        Menu("Budi", true, "DISKON30");
-
-    }
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
@@ -29,6 +25,29 @@ public class Kafe17 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Menu("Andi", true, "DISKON50");
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+
     }
 
 }

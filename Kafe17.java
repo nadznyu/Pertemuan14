3b5ef6f37af04f022f109416cc3f0ec2151a7040
nadznyu <nadiaalya0107@gmail.com>
2025-12-03 19:00:18 +0700

    Modifikasi Percobaan 3 No. 3

--- Kafe17.java
+++ Kafe17.java
@@ -2,20 +2,8 @@ import java.util.Scanner;
 
 public class Kafe17 {
 
-    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
-        System.out.println("Selamat datang, " + namaPelanggan + "!");
+    public static void Menu() {
 
-        if (isMember) {
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        }
-
-        if (kodePromo == "DISKON50") {
-            System.out.println("Berikan diskon 50%");
-        } else if (kodePromo == "DISKON30") {
-            System.out.println("Berikan diskon 30%");
-        } else {
-            System.out.println("Tidak mendapatkan diskon");
-        }
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -27,26 +15,42 @@ public class Kafe17 {
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
 
-    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem, String kodePromo) {
         int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
 
         int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        return hargaTotal;
+        double diskon = 0;
+
+        if (kodePromo.equals("DISKON50")) {
+            diskon = hargaTotal * 0.50;
+            System.out.println("Anda mendapatkan diskon 50%");
+        } else if (kodePromo.equals("DISKON30")) {
+            diskon = hargaTotal * 0.30;
+            System.out.println("Anda mendapatkan diskon 30%");
+        } else {
+            System.out.println("Tidak ada diskon yang diberikan");
+        }
+        int totalSetelahdiskon = (int) (hargaTotal - diskon);
+        return totalSetelahdiskon;
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Menu("Andi", true, "DISKON50");
+        Menu();
 
         System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
         int pilihanMenu = sc.nextInt();
         System.out.print("Masukkan jumlah item yang ingin dipesan: ");
         int banyakItem = sc.nextInt();
 
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+        sc.nextLine();
+        System.out.print("Masukkan kode promo: ");
+        String kodePromo = sc.nextLine();
 
-        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp." + totalHarga);
 
     }
 
